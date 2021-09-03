dec69d509ba131e044bdd5f5d20f06f15d2b9be9
linan31 <linan31@58.com>
2021-09-03 14:48:41 +0800

    feat: vueRouter✨

---
+++ vue-router-study/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  created() {
+    console.log('created')
+  },
+  mounted () {
+    console.log('home====',this.$router)
+  }
+}
+</script>
